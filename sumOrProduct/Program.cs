ab1b7e3af13a6fc16ab1282de48cd41d710575ab
r3adm3 <adrianjfarnell@gmail.com>
2016-01-02 17:55:28 +0000

    First Commit

---
+++ sumOrProduct/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumOrProduct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int usersNumber;
+            string sumOrProduct;
+
+            Console.Write("sumOrProduct\r\n-----------\r\n Enter a number:");
+            usersNumber = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(" Sum or product?");
+            sumOrProduct = Console.ReadLine();
+
+            switch (sumOrProduct.ToLower()){
+                case("sum"):
+                    Console.WriteLine("You've Specified sum: " + (usersNumber + usersNumber));
+                    break;
+                case("product"):
+                    Console.WriteLine("You've Specified Product: " + (usersNumber * usersNumber));
+                    break;
+                default:
+                    Console.WriteLine("you didn't specify, so I'll sum: " + (usersNumber + usersNumber));
+                    break;
+             }
+
+            //debug section
+            Console.WriteLine(usersNumber.ToString() + " " + sumOrProduct);
+            Console.ReadLine();
+
+        }
+    }
+}
